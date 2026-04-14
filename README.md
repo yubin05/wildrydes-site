0c8116f5e7ecef55950be86ada91751b6b6deaa3
yubin05 <vundle34@naver.com>
2026-04-14 12:12:39 +0900

    v1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# wildrydes-site
+# Wild Rydes - AWS Serverless Workshops
+
+This repository contains a collection of workshops and other hands on content that will guide you through building various serverless applications using AWS Lambda, Amazon API Gateway, Amazon DynamoDB, AWS Step Functions, Amazon Kinesis, and other services.
+
+[Workshops](https://github.com/aws-samples/aws-serverless-workshops)
---
+++ frontend/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_15nDMvr2K', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7qjniknkfqefdlmrmvr0m7cu0e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://miq87fm8ef.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
