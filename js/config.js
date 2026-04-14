5ecd08f80e5423363795499f687d49ee95509e40
yubin05 <vundle34@naver.com>
2026-04-14 18:01:05 +0900

    invokeUrl 수정

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://7cpe0i72f2.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://7cpe0i72f2.execute-api.ap-northeast-2.amazonaws.com/prod1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
